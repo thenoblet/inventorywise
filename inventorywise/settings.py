ec79dde33634337d42d05866bcaff47e3a614515
Lawson Israel Pascal <isreallawson@outlook.com>
2024-10-10 16:22:32 +0000

    updated settings.py to include pagination configuration

--- inventorywise/settings.py
+++ inventorywise/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
     'django_filters',
 ]
 
+REST_FRAMEWORK = {
+        'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
