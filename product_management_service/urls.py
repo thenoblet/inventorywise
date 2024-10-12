7f1fd6e4d45d4ba0bafcca19ce7aa006e2bc287e
noblet <appiahpatricknoblet@gmail.com>
2024-10-12 23:52:08 +0100

    Modified urls

--- product_management_service/urls.py
+++ product_management_service/urls.py
@@ -9,8 +9,8 @@ from django.urls import path
 from .views import ProductList, ProductDetail, CategoryList, CategoryDetail
 
 urlpatterns = [
-    path('products/', ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
-    path('categories/', CategoryList.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryDetail.as_view(), name='category-detail'),
+    path('products', ProductList.as_view(), name='product-list'),
+    path('products/<int:pk>', ProductDetail.as_view(), name='product-detail'),
+    path('categories', CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>', CategoryDetail.as_view(), name='category-detail'),
 ]
